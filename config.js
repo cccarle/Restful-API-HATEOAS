ddbd6ee0cf9823fdc6c2a7af380af533ecbdc9c7
Carl <ce222qw@student.lnu.se>
2020-05-13 07:12:33 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  ENV: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT || 4000,
-  URL: process.env.BASE_URL || 'http://localhost:3000',
-  MONGODB_URI:
-    process.env.MONGODB_URI ||
-    'mongodb://restfulUser:Secret56@ds341605.mlab.com:41605/restfulapi',
-  JWT_SECRET: process.env.JWT_SECRET || 'secret'
-}
